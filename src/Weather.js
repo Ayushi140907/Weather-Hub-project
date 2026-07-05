b306ed7765ced4a380a94bef89fc0bf08b4bb42a
Ayushi140907 <pihu.work4@gmail.com>
2026-07-06 01:20:22 +0530

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { MutatingDots } from "react-loader-spinner";
 
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
